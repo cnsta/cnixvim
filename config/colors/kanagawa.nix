3a011fd1049bf7ff96455cd60cf820aa9e4bca07
cnst <cnst@cana.st>
2024-06-22 16:30:05 +0200

    reverted to functionv5

--- config/colors/kanagawa.nix
+++ config/colors/kanagawa.nix
@@ -1,18 +1,18 @@
 _: {
-  base00 = "1F1F28";
-  base01 = "2A2A37";
-  base02 = "223249";
-  base03 = "727169";
-  base04 = "C8C093";
-  base05 = "DCD7BA";
-  base06 = "938AA9";
-  base07 = "363646";
-  base08 = "C34043";
-  base09 = "FFA066";
-  base0A = "DCA561";
-  base0B = "98BB6C";
-  base0C = "7FB4CA";
-  base0D = "7E9CD8";
-  base0E = "957FB8";
-  base0F = "D27E99";
+base00 = "292828"; # bg
+base01 = "32302f"; # bg2
+base02 = "504945"; # bg6
+base03 = "665c54"; # bg8
+base04 = "bdae93"; # p-fg3
+base05 = "ddc7a1"; # fg
+base06 = "ebdbb2"; # p-fg1
+base07 = "fbf1c7"; # p-fg0
+base08 = "ea6962"; # red
+base09 = "e78a4e"; # orange
+base0A = "d8a657"; # yellow
+base0B = "a9b665"; # green
+base0C = "89b482"; # aqua/cyan
+base0D = "7daea3"; # blue
+base0E = "d3869b"; # purple
+base0F = "bd6f3e"; # dim-orange
 }
--- config/plug/colorscheme/colorscheme.nix
+++ config/plug/colorscheme/colorscheme.nix
@@ -11,7 +11,7 @@ in {
       enable = true;
       settings = {
         transparent = true;
-        theme = "dragon";
+        style = "dragon";
       };
     };
     gruvbox = {
