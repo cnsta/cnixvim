ef0e10e815060ff243ba85d56f63e84f434d2717
cnst <cnst@cana.st>
2024-06-22 17:03:34 +0200

    typo

--- config/colors/kanagawa-dragon.nix
+++ config/colors/kanagawa-dragon.nix
@@ -1,18 +1,18 @@
 _: {
-base00 = "292828"; # bg
-base01 = "32302f"; # bg2
-base02 = "504945"; # bg6
-base03 = "665c54"; # bg8
-base04 = "bdae93"; # p-fg3
-base05 = "ddc7a1"; # fg
-base06 = "ebdbb2"; # p-fg1
-base07 = "fbf1c7"; # p-fg0
-base08 = "ea6962"; # red
-base09 = "e78a4e"; # orange
-base0A = "d8a657"; # yellow
-base0B = "a9b665"; # green
-base0C = "89b482"; # aqua/cyan
-base0D = "7daea3"; # blue
-base0E = "d3869b"; # purple
-base0F = "bd6f3e"; # dim-orange
+base00 = "#292828"; # bg
+base01 = "#32302f"; # bg2
+base02 = "#504945"; # bg6
+base03 = "#665c54"; # bg8
+base04 = "#bdae93"; # p-fg3
+base05 = "#ddc7a1"; # fg
+base06 = "#ebdbb2"; # p-fg1
+base07 = "#fbf1c7"; # p-fg0
+base08 = "#ea6962"; # red
+base09 = "#e78a4e"; # orange
+base0A = "#d8a657"; # yellow
+base0B = "#a9b665"; # green
+base0C = "#89b482"; # aqua/cyan
+base0D = "#7daea3"; # blue
+base0E = "#d3869b"; # purple
+base0F = "#bd6f3e"; # dim-orange
 }
